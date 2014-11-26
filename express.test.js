b7c38a05b5253b9feb1a7eeaab6f13bf137492f8
khanghoang <hoangtrieukhang@gmail.com>
2014-11-26 17:46:40 +0700

    Fix the test cases for create user

--- express.test.js
+++ express.test.js
@@ -1,14 +1,43 @@
 var superagent = require("superagent")
 var expect = require("expect.js")
 
-describe('express test api server', function(done){
-    superagent.post('http://localhost:3000/collections/test')
-        .send({
-            name: "John",
-            email: "Khang@gmail.com"
-        })
-        .end(function(e, res){
-            expect(e).to.eql(null)
-            done()
-        })
+var mongoskin = require("mongoskin")
+var express = require('express');
+var app = express();
+
+process.env.NODE_ENV = "test"
+var dbConnectString = 'mongodb://localhost:27017/test'
+var db = mongoskin.db(dbConnectString, {safe: true})
+
+describe('express test api server', function(){
+
+  var DatabaseCleaner 
+  var databaseCleaner
+
+  beforeEach(function(done){
+    DatabaseCleaner = require('database-cleaner')
+    databaseCleaner = new DatabaseCleaner('mongodb')
+    done()
+  })
+
+  afterEach(function(done){
+    databaseCleaner.clean(db, function(e, db){
+      done()
+    })  
+  })
+
+  it('create user', function(done){
+    superagent.post('http://localhost:3000/collections/users')
+    .send({
+      name: "This is very espencial Huy",
+      email: "Khang@gmail.com"
+    })
+    .end(function(e, res){
+      expect(e).to.eql(null)
+      expect(typeof res.body).to.eql('object')
+      done()
+    })
+  })
+
+
 })

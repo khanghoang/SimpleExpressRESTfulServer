3c12136f316dd470561843267496c78267ed8f2f
khanghoang <hoangtrieukhang@gmail.com>
2014-11-29 17:08:46 +0700

    add async and make testing for oauth

--- test/oauth.test.js
+++ test/oauth.test.js
@@ -5,8 +5,8 @@ var mongoskin = require("mongoskin")
 var express = require('express');
 
 var faker = require('faker');
+var async = require('async');
 
-// var databaseModule = require.main.require('./modules/database-module')
 var databaseModule = require('database-module');
 db = databaseModule.db()
 
@@ -20,7 +20,20 @@ describe('Test OAuth 2 server', function(){
   before(function(done){
     DatabaseCleaner = require('database-cleaner')
     databaseCleaner = new DatabaseCleaner('mongodb')
-    db.collection("oauthusers").insert({username: "khang", password: "khang"}, function(){
+
+    // set up test env
+    async.parallel([
+      function(callback){
+        db.collection("oauthusers").insert({username: "khang", password: "khang"}, function(e, db){
+      callback()
+        })
+    },
+      function(callback){
+        db.collection("oauthclients").insert({clientId: "khang", clientSecret: "khang"}, function(e, db){
+        callback()
+        })
+      }],
+    function(err, results){
       done()
     })
   })
@@ -30,14 +43,15 @@ describe('Test OAuth 2 server', function(){
   })
 
   after(function(done){
-    // databaseCleaner.clean(db, function(e, db){
-    //   if(e) console.log(e)
-    //   done()
-    // })  
+    databaseCleaner.clean(db, function(e, db){
+      if(e) console.log(e)
+      done()
+    })  
   })
 
   it('get token', function(done){
     superagent.post('http://localhost:3001/oauth/token')
+    .type("form")
     .send({
       grant_type: "password",
       client_id: "khang",
@@ -46,10 +60,10 @@ describe('Test OAuth 2 server', function(){
       password: "khang"
     })
     .end(function(e, res){
-      console.log("status = " + res.status + res.body.token)
+      // console.log("status = " + res.status + res.body.access_token)
       expect(e).to.eql(null)
       expect(res.status).to.eql(200)
-      expect(res.body.token).to.not.be.an('undefined')
+      expect(res.body.access_token).to.not.be.an('undefined')
       done()
     })
   })
